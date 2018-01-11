762fc1959f5a00b4ea24794ead50fc734901a85f
ccsmart <michael@siebecker.ch>
2018-01-11 23:15:51 +0100

    Add services and startup files

---
+++ recipes-setup/gnss-station/files/pushrawstream.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+MYLOCATION="/tmp/location"
+
+[ -f "${MYLOCATION}" ] || {
+  echo "No location file. Waiting..."
+  sleep 10
+  exit 1
+  }
+
+[ -f /etc/default/transceiver_raw ] || {
+  echo "/etc/default/transceiver_raw missing"
+  sleep 3
+  exit 1
+  }
+
+. ./etc/default/transceiver_raw
+. ".${MYLOCATION}"
+
+echo "Got location Lat: ${LAT} - Lon: ${LON} - Hght: ${HGHT}"
+
+HGHT="`printf '%08.3f' ${HGHT}`"
+
+/usr/bin/transceiver -read_tcp 127.0.0.1 3131 -output_caster pylon.navdata.net 10001 10002 10003 10004 10005 10006 -output_basestation ${LAT} ${LON} RAW ${Status}_UB_${HGHT}_${Name} 1
+
