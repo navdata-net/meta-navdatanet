b383dbc50beb2501e9645a748e8efdbc5c5a3bb4
ccsmart <michael@siebecker.ch>
2018-01-23 22:34:15 +0100

    Add GLONASS in RTCM

--- recipes-setup/gnss-station/files/ubx2rtcm.sh
+++ recipes-setup/gnss-station/files/ubx2rtcm.sh
@@ -9,6 +9,5 @@ LOCFILE="/tmp/location"
 
 source "${LOCFILE}"
 
-#/usr/bin/str2str -in tcpcli://localhost:3131#ubx -out tcpsvr://:8989#rtcm3 -msg "1002(1),1005(10),1006(10),1008(10),1012(1),1013(1),1019(1),1019(1)" -p "${LAT} ${LON} ${HGHT}" -t 1
-/usr/bin/str2str -in tcpcli://localhost:3131#ubx -out tcpsvr://:8989#rtcm3 -msg "1002(1),1005(10),1006(10),1008(10),1013(1),1019(1),1020(1)" -p ${LAT} ${LON} ${HGHT} -t 1
+/usr/bin/str2str -in tcpcli://localhost:3131#ubx -out tcpsvr://:8989#rtcm3 -msg "1002(1),1005(10),1006(10),1008(10),1010(1),1019(1),1020(1),1045(1)" -p ${LAT} ${LON} ${HGHT} -t 1
 
