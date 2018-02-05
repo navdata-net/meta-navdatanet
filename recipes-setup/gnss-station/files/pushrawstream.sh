b76a679f028f9003d13839e0af6a73e94eb8c571
root <root@valle.smartpal.de>
2018-02-05 19:56:55 +0100

    Remove height from station name

--- recipes-setup/gnss-station/files/pushrawstream.sh
+++ recipes-setup/gnss-station/files/pushrawstream.sh
@@ -23,5 +23,5 @@ echo "Got location Lat: ${LAT} - Lon: ${LON} - Hght: ${HGHT}"
 
 HGHT="`printf '%08.3f' ${HGHT}`"
 
-/usr/bin/transceiver -read_tcp 127.0.0.1 3131 -output_caster "${DSTSERVER}" 10001 10002 10003 10004 10005 10006 -output_basestation ${LAT} ${LON} RAW ${Status}_UB_${HGHT}_${Name} 1
+/usr/bin/transceiver -read_tcp 127.0.0.1 3131 -output_caster "${DSTSERVER}" 10001 10002 10003 10004 10005 10006 -output_basestation ${LAT} ${LON} RAW ${Status}_UB_${Name} 1
 
