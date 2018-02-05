6badbdb187e440d5d760252f287ea24e3531362e
root <root@valle.smartpal.de>
2018-02-05 21:02:51 +0100

    Separate decisions for pylon and rtcm 1006

--- recipes-setup/gnss-station/files/locupdate.sh
+++ recipes-setup/gnss-station/files/locupdate.sh
@@ -3,6 +3,7 @@
 CFG="/etc/default/rrd_rtkrcv"
 XYZ="/tmp/xyz"
 export LOCATION="/tmp/location"
+export PYLON="/tmp/pylon"
 
 export MIN="0.000000000"
 
@@ -31,8 +32,23 @@ for QLTY in sol sgl ; do
   [ "${SOLVALS}" -gt "${MINDATA[${WINDOW}]}" ] && break
   done
 
+[ "${QLTY}" = "sgl" -a "${WINDOW}" = "1d" -a "${SOLVALS}" -lt "30" ] && WINDOW="30m"
+
 export WINDOW
 export QLTY
+
+echo "Using >${QLTY}< for window >${WINDOW}<."
+
+TEMP="`rrdtool graph /tmp/test.png --daemon ${RRD} --start +${MARGIN[${WINDOW}]}-${WINDOW} DEF:lat=${DBDIR}/rtkrcv_${QLTY}llh.rrd:Lat:AVERAGE DEF:lon=${DBDIR}/rtkrcv_${QLTY}llh.rrd:Lon:AVERAGE DEF:hght=${DBDIR}/rtkrcv_${QLTY}llh.rrd:Hght:AVERAGE PRINT:lat:AVERAGE:%.8lf PRINT:lon:AVERAGE:%.8lf PRINT:hght:AVERAGE:%.8lf | awk 'NR>1'`"
+
+export NLAT="`echo ${TEMP} | cut -d ' ' -f 1`"
+export NLON="`echo ${TEMP} | cut -d ' ' -f 2`"
+export NHGHT="`echo ${TEMP} | cut -d ' ' -f 3`"
+
+export PLAT="`printf '%.5f' ${NLAT}`"
+export PLON="`printf '%.5f' ${NLON}`"
+
+
 
 killProcesses() {
   PROCNAME="${1:-/usr/bin/transceiver}"
@@ -44,52 +60,80 @@ killProcesses() {
     done
   }
 
-killAll() {
-  killProcesses '/usr/bin/transceiver'
+killLocation() {
+  echo "Killing location process."
   #killProcesses '/usr/bin/str2str'
+  }
+
+killPylon() {
+  echo "Killing pylon transceiver process."
+  killProcesses '/usr/bin/transceiver'
+  }
+
+killAll() {
+  killPylon
+  killLocation
   exit
   }
 
-stopTransmission() {
+stopAll() {
   echo "Deleting ${LOCATION}"
-  [ -f ${LOCATION}  ] && rm -f ${LOCATION}
+  [ -f "${LOCATION}" ] && rm -f "${LOCATION}"
+  [ -f "${PYLON}" ] && rm -f "${PYLON}"
   killAll
   }
 
-
-[ "${QLTY}" = "sgl" -a "${SOLVALS}" -lt "${MINDATA[1d]}" ]  && {
-  echo "Not enough confidence in location."
-  stopTransmission
+zeroOut() {
+  echo "Zero coordinate received."
+  stopAll
   }
 
-echo "Using >${QLTY}< for window >${WINDOW}<."
 
-TEMP="`rrdtool graph /tmp/test.png --daemon ${RRD} --start ${MARGIN[${WINDOW}]}-${WINDOW} DEF:lat=${DBDIR}/rtkrcv_${QLTY}llh.rrd:Lat:AVERAGE DEF:lon=${DBDIR}/rtkrcv_${QLTY}llh.rrd:Lon:AVERAGE DEF:hght=${DBDIR}/rtkrcv_${QLTY}llh.rrd:Hght:AVERAGE PRINT:lat:AVERAGE:%.8lf PRINT:lon:AVERAGE:%.8lf PRINT:hght:AVERAGE:%.8lf | awk 'NR>1'`"
-
-export NLAT="`echo ${TEMP} | cut -d ' ' -f 1`"
-export NLON="`echo ${TEMP} | cut -d ' ' -f 2`"
-export NHGHT="`echo ${TEMP} | cut -d ' ' -f 3`"
 
 [ "${NLAT}" = "nan" -o "${NLON}" = "nan" -o "${NHGHT}" = "nan" ] && {
   echo "nan location data received."
-  stopTransmission
+  stopAll
   }
 
 [ "${NLAT}" = "" -o "${NLON}" = "" -o "${NHGHT}" = "" ] && {
   echo "Empty location data received."
-  stopTransmission
+  stopAll
   }
 
-expr ${NLAT} \< ${MIN} >/dev/null && stopTransmission
-expr ${NLON} \< ${MIN} >/dev/null && stopTransmission
-expr ${NHGHT} \< ${MIN} >/dev/null && stopTransmission
 
-[ -f ${LOCATION} ] && . ${LOCATION}
+expr ${NLAT} \< ${MIN} >/dev/null && zeroOut
+expr ${NLON} \< ${MIN} >/dev/null && zeroOut
+expr ${NHGHT} \< ${MIN} >/dev/null && zeroOut
 
-echo "Writing new ${LOCATION}"
-echo -e LAT=\"${NLAT}\"\\nLON=\"${NLON}\"\\nHGHT=\"${NHGHT}\"\\n > ${LOCATION}
 
-[ "${NLAT}" != "${LAT}" ] && killAll
-[ "${NLON}" != "${LON}" ] && killAll
-[ "${NHGHT}" != "${HGHT}" ] && killAll
+[ -f "${PYLON}" ] && source "${PYLON}"
+
+[ "${PLAT}" != "${LAT}" -o "${PLON}" != "${LON}" ] && {
+  echo "Writing new ${PYLON}"
+  echo -e LAT=\"${PLAT}\"\\nLON=\"${PLON}\"\\n > ${PYLON}
+  killPylon
+  }
+
+[ "${WINDOW}" = "30m" ]  && {
+  echo "Not enough confidence in location."
+  [ -f "${LOCATION}" ] && rm -f "${LOCATION}"
+  killLocation
+  exit
+  }
+
+[ "${QLTY}" = "sgl" -a "${SOLVALS}" -lt "${MINDATA[1d]}" ]  && {
+  echo "Not enough data collected in location."
+  [ -f "${LOCATION}" ] && rm -f "${LOCATION}"
+  killLocation
+  exit
+  }
+
+
+[ -f "${LOCATION}" ] && source "${LOCATION}"
+
+[ "${NLAT}" != "${LAT}" -o "${NLON}" != "${LON}" -o "${NHGHT}" != "${HGHT}" ] && {
+  echo "Writing new ${LOCATION}"
+  echo -e LAT=\"${NLAT}\"\\nLON=\"${NLON}\"\\nHGHT=\"${NHGHT}\"\\n > ${LOCATION}
+  killLocation
+  }
 
