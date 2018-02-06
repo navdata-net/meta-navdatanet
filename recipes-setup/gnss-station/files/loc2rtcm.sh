3be84fafdb117f400cad17ab3bf17ab1b7ec66b8
root <root@valle.smartpal.de>
2018-02-06 21:55:16 +0100

    Actual feeding of RTCM 1006 from location

--- recipes-setup/gnss-station/files/loc2rtcm.sh
+++ recipes-setup/gnss-station/files/loc2rtcm.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-/usr/bin/str2str -in tcpsvr://127.0.0.1:3123#rtcm3 -out tcpsvr://:3133#rtcm3 -msg "1006(10)" -t 1
+export LOCATION="/tmp/location"
 
+[ -f "${LOCATION}" ] || {
+  echo "No location file. Waiting..."
+  sleep 60
+  exit
+  }
+
+source "${LOCATION}"
+
+/usr/bin/str2str -in tcpcli://127.0.0.1:3132#rtcm3 -out tcpcli://127.0.0.1:3123#rtcm3 -msg "1006(10)" -p ${LAT} ${LON} ${HGHT} -s 30000 -t 1
 
